6dd44443161aa9cf5dfc66acbc4a929d8ec5988a
Amélie Guyot <amelieguyot7@gmail.com>
2024-04-12 12:11:35 +0200

    init-post-slice

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import postsReducer from '../store/slice/postSlice';
+//import commentsReducer from '../store/slice/commentSlice';
+
+export const store = configureStore({
+  reducer: {
+    posts: postsReducer,
+    //comments: commentsReducer,
+   
+  },
+});
+
+export default store
