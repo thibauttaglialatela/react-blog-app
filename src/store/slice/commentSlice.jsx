7e795352c90727faffc313052cad09d40b3a6621
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-04-12 15:58:29 +0200

    feat: create CommentSlice

--- src/store/slice/commentSlice.jsx
+++ src/store/slice/commentSlice.jsx
@@ -2,15 +2,16 @@ import axios from "axios";
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 
 export const fetchComments = createAsyncThunk('comments/fetchComments',
-    async (data) => {
-    try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/comments');
-        return response.data;
-    } catch (error) {
-        console.error('error when fetch data', error);
-    }
+    async (postId, {rejectWithValue}) => {
+        try {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`);
+            return response.data;
+        } catch (error) {
+            console.error('error when fetch data', error);
+            return rejectWithValue(error.message);
+        }
     })
-const commentSlice = createSlice( {
+const commentSlice = createSlice({
     name: 'comments',
     initialState: {
         comments: [],
@@ -20,12 +21,29 @@ const commentSlice = createSlice( {
             name: "",
             email: "",
             body: ""
-        }
+        },
+        loading: false,
+        error: null
     },
     reducers: {
         setComment(state, action) {
             state.comment = action.payload
         }
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchComments.pending, state => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchComments.fulfilled, (state, action) => {
+                state.loading = false;
+                state.comments = action.payload;
+            })
+            .addCase(fetchComments.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            })
     }
 })
 
